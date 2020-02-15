6b669c24b1872d42fbc4ee8a0708d95f44892f3b
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-15 16:43:19 +0530

    ADDED:Selection Statement To Display Head Or Tail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,3 +2,17 @@
 
 #DISPLAYING WELCOME MESSAGE
 echo "Welcome To Flip Coin Simulation"
+
+#VARIABLES
+result=0;
+
+#GENERATING VALUE USING RANDOM
+result=$(( RANDOM%2 ))
+
+#CHECKING CONDITION
+if [ $result -eq 0 ]
+then
+	echo "It's a Head"
+else
+	echo "It's a Tail"
+fi
