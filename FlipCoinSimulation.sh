e4cb232b8ed03b16ce7b62073deacafa9f4b90ee
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-15 17:53:43 +0530

    ADDED:Dictionary To Store Result And Calculated Percentage Of Singlet Combination

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -3,16 +3,32 @@
 #DISPLAYING WELCOME MESSAGE
 echo "Welcome To Flip Coin Simulation"
 
+#DECLEARING DICTIONARY
+declare -A flipResult
+
 #VARIABLES
 result=0;
+flipTimes=0;
+countHead=0;
+countTail=0;
+percentageHead=0;
+percentagetail=0;
 
-#GENERATING VALUE USING RANDOM
-result=$(( RANDOM%2 ))
+#TAKING USER INPUT OF NUMBER OF TIMES
+read -p "Enter The Number Of Times You Want To Flip A Coin" flipTimes
 
-#CHECKING CONDITION
-if [ $result -eq 0 ]
-then
-	echo "It's a Head"
-else
-	echo "It's a Tail"
-fi
+#GENERATING VALUE AND STORING IN DICTIONARY
+for(( index=1; index<=flipTimes; index++ ))
+do
+	result=$(( RANDOM%2 ))
+	if [ $result -eq 0 ]
+	then
+		flipResult[head]=$((++countHead))
+	else
+		flipResult[tail]=$((++countTail))
+	fi
+done
+
+#CALCULATING PERCENTAGE OF HEAD AND TAIL
+percentageHead=`echo "scale=2; ${flipResult[head]}/$flipTimes*100" | bc`
+percentageTail=`echo "scale=2; ${flipResult[tail]}/$flipTimes*100" | bc`
