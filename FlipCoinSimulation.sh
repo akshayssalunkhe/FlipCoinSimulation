2c82a3f29f40bd71fc5b69d1563861e5d0b3474b
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-17 15:14:45 +0530

    ADDED:Variables And Function To Generate Dublet Combination,And Stored Result With It's percentage

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -6,29 +6,49 @@ echo "Welcome To Flip Coin Simulation"
 #DECLEARING DICTIONARY
 declare -A flipResult
 
+#CONSTANT
+numberOfCoins=2;
+
 #VARIABLES
-result=0;
 flipTimes=0;
-countHead=0;
-countTail=0;
-percentageHead=0;
-percentagetail=0;
+side=0;
+index=0;
 
 #TAKING USER INPUT OF NUMBER OF TIMES
-read -p "Enter The Number Of Times You Want To Flip A Coin" flipTimes
+read -p "Enter The Number Of Times You Want To Flip A Coin " flipTimes
 
 #GENERATING VALUE AND STORING IN DICTIONARY
-for(( index=1; index<=flipTimes; index++ ))
-do
-	result=$(( RANDOM%2 ))
-	if [ $result -eq 0 ]
+function flip() {
+	if [[ $flipTimes -gt 0 ]]
 	then
-		flipResult[head]=$((++countHead))
+		for (( i=1; i<=flipTimes; i++ ))
+		do
+			side=""
+			for(( index=1; index<=$numberOfCoins; index++ ))
+			do
+				if [ $((RANDOM%2)) -eq 0 ]
+				then
+					side+=H
+				else
+					side+=T
+				fi
+			done
+			flipResult[$side]=$((${flipResult[$side]}+1))
+		done
 	else
-		flipResult[tail]=$((++countTail))
+		echo "Enter Value Greater Than Zero"
 	fi
+}
+
+#CALLING FUNCTION
+flip $(( $flipTimes ))
+
+#USING LOOP TO CALCULATE PERCENTAGE
+for key in ${!flipResult[@]}
+do
+	flipResult[$key]=`echo  "scale=2; ${flipResult[$key]}*100/$flipTimes" | bc`
 done
 
-#CALCULATING PERCENTAGE OF HEAD AND TAIL
-percentageHead=`echo "scale=2; ${flipResult[head]}/$flipTimes*100" | bc`
-percentageTail=`echo "scale=2; ${flipResult[tail]}/$flipTimes*100" | bc`
+#DISPLAYING VALUES AND KEYS
+echo ${flipResult[@]}
+echo ${!flipResult[@]}
